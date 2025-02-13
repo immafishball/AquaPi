4d542b252cdb857eea4306ce89e8c086912129ff
immafishball <hansmarcos14@gmail.com>
2025-02-13 18:36:27 +0800

    Use the global ADS115

--- AquaPi/feeder.py
+++ AquaPi/feeder.py
@@ -4,41 +4,12 @@ import RPi.GPIO as GPIO
 import os
 import time
 import sys
+from sensor_manager import get_board
 
 DATABASE = '/home/pi/Projects/AquaPi/sensor_data.db'
 
 SERVO_PIN = 5
 GPIO.setmode(GPIO.BCM)
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
-
-from DFRobot_RaspberryPi_Expansion_Board import DFRobot_Expansion_Board_IIC as Board
-
-board = Board(1, 0x10)    # Select i2c bus 1, set address to 0x10
-
-def board_detect():
-  l = board.detecte()
-  print("Board list conform:")
-  print(l)
-
-''' print last operate status, users can use this variable to determine the result of a function call. '''
-def print_board_status():
-  if board.last_operate_status == board.STA_OK:
-    print("board status: everything ok")
-  elif board.last_operate_status == board.STA_ERR:
-    print("board status: unexpected error")
-  elif board.last_operate_status == board.STA_ERR_DEVICE_NOT_DETECTED:
-    print("board status: device not detected")
-  elif board.last_operate_status == board.STA_ERR_PARAMETER:
-    print("board status: parameter error")
-  elif board.last_operate_status == board.STA_ERR_SOFT_VERSION:
-    print("board status: unsupport board framware version")
-
-board_detect()
-# Pwm channel need external power
-board.set_pwm_enable()        
-# board.set_pwm_disable()
-board.set_pwm_frequency(1000) 
 
 def create_table():
     conn = sqlite3.connect(DATABASE)
@@ -54,6 +25,8 @@ def create_table():
 
 # Add the following line to ensure the table exists
 create_table()
+
+board = get_board()
 
 def feed_now():
     try:
