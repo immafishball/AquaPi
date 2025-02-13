c16df976999c0bf2abd26e3c8ddca1d1e5df3eb6
immafishball <hansmarcos14@gmail.com>
2025-02-13 13:33:37 +0800

    Missing additional functions

--- AquaPi/sensor_manager.py
+++ AquaPi/sensor_manager.py
@@ -8,8 +8,11 @@
   @url https://github.com/DFRobot/DFRobot_PH
 '''
 import sys
-sys.path.append('../')
 import time
+
+sys.path.append('../')
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+
 ADS1115_REG_CONFIG_PGA_6_144V        = 0x00 # 6.144V range = Gain 2/3
 ADS1115_REG_CONFIG_PGA_4_096V        = 0x02 # 4.096V range = Gain 1
 ADS1115_REG_CONFIG_PGA_2_048V        = 0x04 # 2.048V range = Gain 2 (default)
@@ -18,6 +21,7 @@ ADS1115_REG_CONFIG_PGA_0_512V        = 0x08 # 0.512V range = Gain 8
 ADS1115_REG_CONFIG_PGA_0_256V        = 0x0A # 0.256V range = Gain 16
 
 from DFRobot_ADS1115 import ADS1115
+from DFRobot_RaspberryPi_Expansion_Board import DFRobot_Expansion_Board_IIC as Board
 
 ads1115 = ADS1115()
 
@@ -29,3 +33,33 @@ ads1115.setGain(ADS1115_REG_CONFIG_PGA_6_144V)
 #Access the initialized ADS1115 instance
 def get_ads1115():
     return ads1115
+
+board = Board(1, 0x10)    # Select i2c bus 1, set address to 0x10
+
+def board_detect():
+  l = board.detecte()
+  print("Board list conform:")
+  print(l)
+
+''' print last operate status, users can use this variable to determine the result of a function call. '''
+def print_board_status():
+  if board.last_operate_status == board.STA_OK:
+    print("board status: everything ok")
+  elif board.last_operate_status == board.STA_ERR:
+    print("board status: unexpected error")
+  elif board.last_operate_status == board.STA_ERR_DEVICE_NOT_DETECTED:
+    print("board status: device not detected")
+  elif board.last_operate_status == board.STA_ERR_PARAMETER:
+    print("board status: parameter error")
+  elif board.last_operate_status == board.STA_ERR_SOFT_VERSION:
+    print("board status: unsupport board framware version")
+
+board_detect()
+# Pwm channel need external power
+board.set_pwm_enable()        
+# board.set_pwm_disable()
+board.set_pwm_frequency(1000) 
+
+#Access the initialized Expansion Board instance
+def get_board():
+    return board
