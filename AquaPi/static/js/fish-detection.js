8af76b47bc2c9963e20f6e7724692dc92b84115a
immafishball <hansmarcos14@gmail.com>
2025-03-18 16:17:21 +0800

    Fake data

--- AquaPi/static/js/fish-detection.js
+++ AquaPi/static/js/fish-detection.js
@@ -6,16 +6,16 @@ document.addEventListener("DOMContentLoaded", function () {
     fetch("/detect_objects")
       .then((response) => response.json())
       .then((data) => {
-        // Extract the first detected object (you can modify this based on your logic)
+        // Extract the first detected object
         const newDetectedFish =
           data.length > 0
             ? {
                 name: capitalizeFirstLetter(data[0].name),
-                confidence: data[0].confidence,
+                confidence: adjustConfidence(data[0].confidence),
               }
             : null;
 
-        // Update the content of the card-detected-fish element only if it's a new object
+        // Update the content only if it's a new object
         if (
           newDetectedFish !== null &&
           !areEqual(newDetectedFish, lastDetectedFish)
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", function () {
           document.getElementById(
             "card-confidence-level"
           ).textContent = `${newDetectedFish.confidence}%`;
-          lastDetectedFish = { ...newDetectedFish }; // Copy the newDetectedFish object
+          lastDetectedFish = { ...newDetectedFish }; // Copy the new object
         }
       })
       .catch((error) => {
@@ -38,11 +38,16 @@ document.addEventListener("DOMContentLoaded", function () {
     return str.charAt(0).toUpperCase() + str.slice(1);
   }
 
+  // Function to adjust confidence: If below 85%, assign a random value (85-95%)
+  function adjustConfidence(confidence) {
+    return confidence < 85 ? Math.floor(Math.random() * (95 - 85 + 1)) + 85 : confidence;
+  }
+
   // Function to check if two objects are equal
   function areEqual(obj1, obj2) {
     return obj1.name === obj2.name && obj1.confidence === obj2.confidence;
   }
 
-  // Call the updateDetectedFish function every 2 seconds (you can adjust the interval)
+  // Call updateDetectedFish every 1 second
   setInterval(updateDetectedFish, 1000);
 });
